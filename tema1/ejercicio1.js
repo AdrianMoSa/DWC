dc0639a12e519fe087c112100550d3e599a832db
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-09-20 20:03:23 +0200

    ejercicio1 mejorado

--- tema1/ejercicio1.js
+++ tema1/ejercicio1.js
@@ -1,18 +1,14 @@
 "use strict";
-let masa;
-let altura;
+
 let masaJuan=70;
 let alturaJuan=170;
 let alturaMarcos=200;
 let masaMarcos=150;
-let IMC=masa/(altura*altura);
-
 altura=alturaJuan;
 masa=masaJuan;
+let IMCJuan=masaJuan/(alturaJuan*alturaJuan);
+let IMCMarcos=masaMarcos/(alturaMarcos*alturaMarcos);
 
-console.log(IMC);
-
-altura=alturaMarcos;
-masa=masaMarcos;
-console.log(IMC);
+console.log(IMCJuan);
+console.log(IMCMarcos);
 
