096381bc4be2b6769b0ce5be8c11e53f71692655
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-09-20 20:04:33 +0200

    commit bueno de verdad

--- tema1/ejercicio1.js
+++ tema1/ejercicio1.js
@@ -4,8 +4,6 @@ let masaJuan=70;
 let alturaJuan=170;
 let alturaMarcos=200;
 let masaMarcos=150;
-altura=alturaJuan;
-masa=masaJuan;
 let IMCJuan=masaJuan/(alturaJuan*alturaJuan);
 let IMCMarcos=masaMarcos/(alturaMarcos*alturaMarcos);
 
