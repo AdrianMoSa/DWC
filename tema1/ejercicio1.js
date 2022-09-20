f57ce10cf1771ed203f2128fcaee6a11d0b139f7
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-09-20 19:58:50 +0200

    ejercicio1 entornoCliente

---
+++ tema1/ejercicio1.js
@@ -0,0 +1,18 @@
+"use strict";
+let masa;
+let altura;
+let masaJuan=70;
+let alturaJuan=170;
+let alturaMarcos=200;
+let masaMarcos=150;
+let IMC=masa/(altura*altura);
+
+altura=alturaJuan;
+masa=masaJuan;
+
+console.log(IMC);
+
+altura=alturaMarcos;
+masa=masaMarcos;
+console.log(IMC);
+
