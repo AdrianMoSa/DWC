6ad2be205fedf1b37fb018bdf26f3b8d07595523
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-09-28 13:45:20 +0200

    fatalpush

--- practica2.05/ejercicio2.js
+++ practica2.05/ejercicio2.js
@@ -1,8 +1,9 @@
 "use script";
-//var mytimeout=setTimeout(temporizador(2,25),10000);
+
 function temporizador(minutos,segundos){
     var totalSegundos=(minutos*60)+segundos;
-for(var i=0;i<totalSegundos;setInterval(i++,1000)){
+    console.log(`Quedan un total de ${minutos} minutos y ${segundos} segundos `);
+for(var i=0;i<totalSegundos;setTimeout(i++,1000)){
     if(segundos==0){
         segundos=60;
         minutos--;
@@ -13,6 +14,7 @@ for(var i=0;i<totalSegundos;setInterval(i++,1000)){
 }
 
 }
+//var mytimeout=setTimeout(temporizador(2,25),1000);
 temporizador(2,25);
 /*
 var s = 1;
