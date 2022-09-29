481c684f21defbb50f9308d7051d286eea3eee09
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-09-29 13:41:32 +0200

    ejercicio1y2terminado

--- practica2.05/ejercicio1.js
+++ practica2.05/ejercicio1.js
@@ -1,18 +1,28 @@
 "use strict";
+//Función que sirve para pasar una frase a cani.
 
 function tocani(frase){
-var array=Array.from(frase);
 var arrayFinal=[];
+var letra="H";
+var intervalo=Math.floor((Math.random() * (5 - 2 + 1)) + 2);
 
-   for(var i=0;i<array.length;i++){
+
+   for(var i=0;i<frase.length;i++){
     if(i%2==0){
         arrayFinal[i]=frase[i].toUpperCase();
     }
     else{
         arrayFinal[i]=frase[i].toLowerCase();
     }
-    
+    if(frase[i]==="c"){
+        arrayFinal[i]="k";
     }
-    return console.log(arrayFinal);
+    if(frase.charAt(i)==="C"){
+        arrayFinal[i]="K";
+    }
+
+    }
+    arrayFinal.push(letra.repeat(intervalo))
+    return console.log(arrayFinal.join(""));
 }
-tocani("ESCATERGORIs");
+tocani("Calcetines y medias");
--- practica2.05/ejercicio2.js
+++ practica2.05/ejercicio2.js
@@ -1,5 +1,6 @@
 "use script";
 
+//Programa que realiza un decremento de minutos y segundos. Mediante setInterval se ejecuta el decremento cada segundo.
 
 var minutos=1;
 var segundos=5;
@@ -9,12 +10,20 @@ const id=setInterval(temporizador,1000);
 
 
 function temporizador(){
+ 
   terminar=false;
-
+  if(isNaN(minutos)||isNaN(segundos)){
+    console.log("Datos incorrectos: debes introducir números. ");
+  }
+  else if(minutos<0||segundos>59||segundos<0){
+    console.log("Datos incorrectos: debes introducir los números correctamente.")
+  }
+  else{
     if(segundos==0){
         segundos=60;
         minutos--;
     }
+    
     
     segundos--;
     
@@ -23,8 +32,8 @@ function temporizador(){
     if(segundos===0&&minutos==0){
       clearInterval(id);
     }
-  
   }
+}
 
 
 
