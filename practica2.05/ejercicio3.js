3a1609d888e59434cd00ca087e02b3fc236f86f7
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-09-29 18:12:43 +0200

    ejercicio3casi

--- practica2.05/ejercicio3.js
+++ practica2.05/ejercicio3.js
@@ -3,10 +3,21 @@
 const id=setInterval(solicitarDni,10000);
 
 function solicitarDni(){
-var dni=prompt("Introduce tu dni sin letra: ");
+var secuenciaLetrasNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+var dni=[prompt("Introduce tu dni sin letra: ")];
+var letraDni=[];
+var resultado;
 
 if(dni==-1){
     clearInterval(id);
+    }
+    else{
+        for(var i=0;i<dni.length;i++){
+            resultado=dni%23;
+            letraDni[i]=secuenciaLetrasNIF.charAt(resultado);
+            
+            }
+    }
+    return console.log(`La letra del dni: ${dni} es ${letraDni}`);
 }
 
-}
