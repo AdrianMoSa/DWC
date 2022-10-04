e8bc153d2a5d54f586da33ff9e4b5eb6e33f1412
AdrianMoSa <adrianmoyasanchez@gmail.com>
2022-10-04 12:04:32 +0200

    no me funca el sudoki

--- practica2.04/ejercicio1.js
+++ practica2.04/ejercicio1.js
@@ -23,22 +23,35 @@ sudokuIncorrecto[7] = [8, 9, 7, 2, 3, 1, 5, 6, 4];
 sudokuIncorrecto[8] = [5, 6, 4, 8, 9, 7, 2, 3, 1];
 
 function comprobarSudoku(sudoku) {
-  
+  var arrayComprobar=[]
   var contador=0;
   var correcto = true;
-  
+  var numeroComprobar;
   for (var i = 0; i < sudoku.length; i++) {
-    
+    arrayComprobar[i]=sudoku[i][0];    
     //Bucle que recorre el array que está en la posición i
     for (var j = 0; j < sudoku[i].length; j++) {
-        
-        
-       
-        
+               
+   
       
     }
+  }
+  for(var i=0;i<arrayComprobar.length;i++){
+    console.log(arrayComprobar[i]);
+    numeroComprobar=arrayComprobar[i]
+    for(var j=0;i<arrayComprobar.length;j++){
+     
+      if(numeroComprobar===arrayComprobar[i]){
+        contador++;
+  
+      }
+      if(contador>2){
+        return console.log("el array esta mal");
+      }
+    }
+    
   }
   return(console.log("Todo ok Jose Luis"));
 }
 
-comprobarSudoku(sudokuIncorrecto);
+comprobarSudoku(sudokuCorrecto);
