90b2f97ca728ed4d26740060f672fedbfb274bde
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-10-11 19:55:58 +0200

    ejercicio1casiterminao

---
+++ practica3.01/ejercicio1/ejercicio1.js
@@ -0,0 +1,5 @@
+"use strict"
+let numeroParrafos= document.querySelectorAll("p");
+let textoSegundoParrafo= document.querySelector("p:nth-chlid(2)");
+let numeroEnlacesPagina=document.querySelectorAll("a").length;
+let direccionPrimerEnlace=document.querySelector("a:first-child").getAttribute("href");
