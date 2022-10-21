4219d95f9595838a9e09321918f4d1e78fdb2f33
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-10-21 20:44:25 +0200

    sadadasd

--- practica3.01/practica 3.03/js/gestor.js
+++ practica3.01/practica 3.03/js/gestor.js
@@ -6,18 +6,44 @@ function generarTarea(){
     var texto=doc.getElementById("tareas").firstElementChild;
     tarea.classList.add('tarea');
     tarea.innerHTML=(`
-    <p>{texto}</p>
+    <p>${texto.value}</p>
     <p class="botones">
       <input type="button" value="Borrar" class="del" />
       <input type="button" value="Acabar" class="end" />
     </p>`);
+    
+   
     return tarea;
 }
+
+
 var anyadirTarea=doc.querySelector('.add')
 
 anyadirTarea.addEventListener('click',() => {
    var destino=doc.getElementById("pendientes");
+   var texto=doc.getElementById("tareas").firstElementChild;
+   if(texto.value==""){
+    texto.value="Escriba algo señor";
+  }
+  else{
     destino.appendChild(generarTarea());
+    
+  }
+    texto.value="";
+    texto.focus();
 })
+function deletearTarea(a){
+  console.log(a)
+// var almacen=doc.querySelector('.del')
+a.parentElement.parentElement.remove();
+// almacen.remove();
+
+}
+
+
+var borrarTarea=doc.querySelector('.del')
+borrarTarea.addEventListener('click',()=>{
+  deletearTarea(this);
+});
 
 
