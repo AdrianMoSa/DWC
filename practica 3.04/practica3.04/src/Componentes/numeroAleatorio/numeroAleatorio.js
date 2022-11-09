ccf92eb09a2f891b0c584bef3a87e74b9e8a891a
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-11-09 18:53:52 +0100

    asdada

---
+++ practica 3.04/practica3.04/src/Componentes/numeroAleatorio/numeroAleatorio.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+function numeroAleatorio() {
+  const valorInicial = [];
+  const [numerosAleatorios, setNumeroAleatorio] = useState(valorInicial);
+
+  const addNumeroAleatorio = (aleatorio) => {
+    //meter la función del numero aleatorio, supongo que la puedes declarar fuera y las comprobaciones de si está o no, si está llamar de nuevo a la función.
+    setNumeroAleatorio([...numerosAleatorios, aleatorio]);
+  };
+
+  const borrarTodo = () => {
+    setNumeroAleatorio([]);
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <p>
+        {numerosAleatorios.map((numeroAleatorio) => {
+            return <li key={numeroAleatorio}>{numeroAleatorio}</li>;
+          })}
+        </p>
+        <button
+          onClick={() => {
+            addNumeroAleatorio();
+          }}
+        >
+          Generar
+        </button>
+        <button
+          onClick={() => {
+            borrarTodo();
+          }}
+        >
+          Eliminar
+        </button>
+      </div>
+    </React.Fragment>
+  );
+}
+export default numeroAleatorio;
