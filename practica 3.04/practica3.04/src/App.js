dc63543c758b301155649a441ff390f553cabc30
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-11-10 15:56:05 +0100

    se sube

--- practica 3.04/practica3.04/src/App.js
+++ practica 3.04/practica3.04/src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import './App.css';
 import { Contenedor } from './Componentes/Contenedor/Contenedor';
+import NumeroAleatorio from './Componentes/NumeroAleatorio/NumeroAleatorio';
+
 
 function App() {
   return (
     <div className="App">
-     <Contenedor></Contenedor>
+    {<NumeroAleatorio/>}
     </div>
   );
 }
