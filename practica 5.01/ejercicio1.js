353a01853833b82bad291e92103203ba081c5085
AdrianMoSA <adrianmoyasanchez@gmail.com>
2022-11-17 16:05:45 +0100

    codigo que funciona

--- practica 5.01/ejercicio1.js
+++ practica 5.01/ejercicio1.js
@@ -1,11 +1,12 @@
 "use strict";
-import { mostrarPelicula } from "./Bibliotecas/manipularDom";
-import { getPeliculas } from "./Bibliotecas/obtenerDatos";
+import { mostrarPelicula } from "./Bibliotecas/manipularDom.js";
+import { getPeliculas } from "./Bibliotecas/obtenerDatos.js";
 window.onload = () => {
-    let cartelera=document.getElementById("pelicula");
+    var cartelera=document.getElementById("pelicula");
     const url = "http://swapi.py4e.com/api/films/";
+    let boton=document.getElementById("contenedor");
 
-      window.addEventListener("load", () => {
+      boton.addEventListener("click", () => {
       // Se obtiene una promesa, no los datos que contiene.
       console.log(getPeliculas(url));
       // Se obtiene una promesa y se consume para acceder a su contenido.
